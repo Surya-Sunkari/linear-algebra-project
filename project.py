13756ec08bc19fdedaac69845556ccbf14a3a31a
Surya-Sunkari <88792523+Surya-Sunkari@users.noreply.github.com>
2022-02-17 13:59:06 -0600

    Update project.py
    more user friendly and attempting to fix mathematical issues with determinant calculation

--- project.py
+++ project.py
@@ -24,25 +24,24 @@ def get_vectors_2d():
     vectors = []
     
     while True:
-        kb = input('enter a 2d vector separated by a space, or "done" when finished:  ')
+        kb = input('Enter a 2d vector separated by a space, or "done" when finished:  ')
         if kb == 'done':
             break
         kb = kb.split()
         try:
             vectors.append([int(kb[0]), int(kb[1])])
         except:
-            print('problem with your input. make sure to follow "x y" format')
+            print('Problem with your input. Make sure to follow the "x y" format.')
         else:
-            print('vector appended')
+            print('Vector successfully appended.')
     
     return vectors
 
 def vector_grapher_2d():
     M = np.array(get_vectors_2d())
     colors = list(mcolors.BASE_COLORS)
-    print(len(colors))
     
-    print(M, 'M')
+    print('\nNow graphing:\n', M)
     
     rows,cols = M.T.shape
     
@@ -50,46 +49,44 @@ def vector_grapher_2d():
     maxes = 1.1*np.amax(M, axis = 0)
     mins = 1.1*np.amin(M, axis=0)
     
-    print(maxes, 'maxes')
-    print(mins, 'mins')
-    
     for i,l in enumerate(range(0,cols)):
         #xs = [0,M[i,0]]
         #ys = [0,M[i,1]]
         #plt.plot(xs,ys)
         plt.arrow(0, 0, M[i,0], M[i,1], length_includes_head=True, head_width=0.4, width=0.1, facecolor=colors[i], edgecolor=colors[i])
-    
+        plt.annotate(rf'$V_{i}$', xy=(M[i,0], M[i,1]))
+        
     xmax = max(np.ceil(maxes[0]) + 1, 1)
     ymax = max(np.ceil(maxes[1]) + 1, 1)
     xmin = min(np.floor(mins[0]) - 1, -1)
     ymin = min(np.floor(mins[1]) - 1, -1)
     
-    print(xmin, xmax, 'xbounds')
-    print(ymin, ymax, 'ybounds')
+    print(f'\nx-bounds: [{xmin}, {xmax}]')
+    print(f'y-bounds: [{ymin}, {ymax}]')
     
     plt.plot(0,0, 'ok') # plot a black point at the origin
     plt.xlim((xmin, xmax)) # set the x axis limits
     plt.ylim((ymin, ymax)) # set the y axis limits
-    plt.legend(['V'+str(i+1) for i in range(cols)]) # give a legend
+    #plt.legend(['V'+str(i+1) for i in range(cols)]) # give a legend
+      
     plt.grid(visible=True, which='major') # plot grid lines
     plt.show()
-
     
 def print_matrix(matrix):
     for row in matrix:
         print(row)
     
 def determinant_value_inputer():
-    n = int(input('input the size of your matrix: '))
+    n = int(input('\nInput the size of your matrix: '))
     
     matrix = []
     for row in range(1, n+1):
         row_list = []
         for col in range(1, n+1):
-            row_list.append(int(input(f'value at row {row}, col {col}: ')))
+            row_list.append(int(input(f'Value at row {row}, col {col}: ')))
         matrix.append(row_list)
     
-    print('\nthe determinant of the matrix\n')
+    print('\nThe determinant of the matrix\n')
     print_matrix(matrix)
     print(f'\nis {determinant_calculator(n, matrix)}')
 
@@ -110,12 +107,24 @@ def determinant_calculator(n, matrix):
                         row_list.append(matrix[row][col])
                 new_matrix.append(row_list)
             new_matrix.pop(0)
-            print(new_matrix)
-            c = multiplier * a * determinant_calculator(n-1, new_matrix)
+            c = multiplier * matrix[0][a] * determinant_calculator(n-1, new_matrix)
             c_list.append(c)
             multiplier *= -1
         return sum(c_list)
+
+
+contin = True
+print('Welcome to the Linear Algebra Operations Calculator/Visualizer.')
+while(contin):
+    kb = input('\nWhat would you like to do? (enter the letter):\n\na) graph 2d vectors\nb) calculate determinant\nc) exit program\n\n')
+    if kb == 'a':
+        vector_grapher_2d()
+    elif kb == 'b':
+        determinant_value_inputer()
+    elif kb == 'c':
+        contin = False
+    else:
+        print('Unknown Input, please try again.\n')
+
+print('\nHave a great day!')
     
-#vector_grapher_2d()
-determinant_value_inputer()
-#print_matrix([[1,2,3],[4,25,6],[17,8,9]])
